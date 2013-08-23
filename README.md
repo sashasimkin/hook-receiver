d929605d648006ec3d09801653fa96251c091a8a
Sasha Simkin <sashasimkin@gmail.com>
2013-08-24 02:16:44 +0300

    Readme, improvements

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-hook-receiver.py
-================
+About
+===
+Git [web-hook](https://help.github.com/articles/post-receive-hooks) receiver written in python.
+Created for prevent a routine work, such as working with database, static files, etc., after pushing changes to repo.
 
-Git web-hook receiver written in python :-)
+System requirements
+===
+* python>=2.6(Tested on 2.7)
+* OS: Linux(tested on Ubuntu), *BSD(Not tested), Windows(But have small bugs) or otherwise where can be runned python
+
+Instalation
+===
+1. `git clone git://github.com/sashasimkin/hook-receiver.py.git`
+2. `cd hook-reciever.py/`
+3. `virtualenv .env`
+4. `pip install -r requirements.txt`
+5. `chmod 0777 logs/`
+
+Usage
+===
+* See the `config` section in this file and `config/samle.py`
+* run server `python app.py` or use builtin `wsgi.py` as wsgi handler for your front server
+* Use `http://IP:5000/{cofig_name}`(or else you configured) as hook url in github, gitlab, etc.
+
+Config
+===
+Single config is a python module, which must be placed in `config` package and contains variables below.
+
+* `PATH` - Root path for project, shell commands has been executed here
+* `COMMANDS` - Array of shell commands, which will be performed after recieve hook. Default: `COMMANDS = []`
+* `REFS` - List of patterns payload.ref to match for repform actions, otherwise - nothing hapens. Default `REFS = ['.*']`
+ 
+TODO
+===
+* Pre-processing for COMMANDS
--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import os
 import re
 import json
 import logging
-import logging.handlers
 from subprocess import Popen, PIPE
 from flask import Flask, request
-app = Flask(__name__)
 
 PATH = os.path.abspath(os.path.dirname(__file__))
-LOG_PATH = os.path.join(PATH, 'logs')
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -21,15 +19,15 @@ def receive(cfg_name):
     def spawn_logger(cfg, logger_name):
         """
         Get logger instance for config_name and logger name
-        :param cfg:
-        :param logger_name:
+        :param cfg: Current config name
+        :param logger_name: Logger name, now error and info
         :return:
         """
         logger = logging.getLogger(logger_name)
         logger.setLevel(logging.INFO)
-        
+
         handler = logging.handlers.RotatingFileHandler(
-            os.path.join(LOG_PATH, '%s.%s.log' % (cfg, logger_name)),
+            os.path.join(PATH, 'logs/%s.%s.log' % (cfg, logger_name)),
             backupCount=5
         )
         handler.setFormatter(logging.Formatter(u'[%(asctime)s]  %(message)s'))
@@ -47,9 +45,23 @@ def receive(cfg_name):
     try:
         cfg = getattr(__import__('config.%s' % cfg_name, globals(), locals(), level=-1), cfg_name)
 
-        refs = getattr(cfg, 'REFS', [r'.*'])
-        if not isinstance(refs, list):
-            raise TypeError('REFS must be a list')
+        def get_variable(name, default, condition, fail_message):
+            v = getattr(cfg, name, default)
+            if condition(v):
+                raise RuntimeError(fail_message)
+
+        path = get_variable('PATH',
+                            '',
+                            lambda p: not p or not os.path.exists(p),
+                            'PATH does not exists')
+        refs = get_variable('REFS',
+                            [r'.*'],
+                            lambda r: not isinstance(r, list),
+                            'REFS must be a list')
+        commands = get_variable('COMMANDS',
+                                [],
+                                lambda c: not isinstance(c, list),
+                                'Define COMMANDS variable, else nothing happens')
 
         for ref in refs:
             refExpr = re.compile(ref, re.IGNORECASE)
@@ -58,21 +70,17 @@ def receive(cfg_name):
                 info_logger.info('Ref does not fit')
                 return ''
 
-        for command in getattr(cfg, 'COMMANDS', []):
-            print 'will run: %s' % command
-            pipe = Popen(command,
-                         shell=True,
-                         cwd=os.path.join(PATH, cfg.PATH),
-                         # close_fds=True,
-                         stdout=PIPE,
-                         stderr=PIPE)
-            out, err = pipe.communicate()
+        for command in commands:
+            # Execute current command and log out and errors
+            out, err = Popen(command,
+                             shell=True,
+                             cwd=os.path.join(PATH, cfg.PATH),
+                             # close_fds=True,
+                             stdout=PIPE,
+                             stderr=PIPE).communicate()
             if out:
-                print 'get stdout', out.encode('utf8')
                 info_logger.info(out.encode('utf8'))
-
             if err:
-                print 'get stderr %s' % err.encode('utf8')
                 error_logger.error(err.encode('utf8'))
 
     except Exception, e:
